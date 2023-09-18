52595c6779bcb2b99a7367f4bc7b8331134422ce
Angie <angie.monnye@gmail.com>
2023-09-18 10:58:40 +0200

    Update the landing page

--- setup_mysql_zkhp.sql
+++
@@ -1,7 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS zkhipani;
-CREATE USER IF NOT EXISTS 'zkhp_dev'@'localhost' IDENTIFIED BY 'zkhp_pwd';
-GRANT ALL PRIVILEGES ON `zkhipani`.* TO 'zkhp_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'zkhp_dev'@'localhost';
-FLUSH PRIVILEGES;
